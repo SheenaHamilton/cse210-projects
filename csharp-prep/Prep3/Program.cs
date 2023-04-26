23cf20106c137bfe5934061fbfd9be7138980fe3
sheenahamilton <sheenhamilton@gmail.com>
2023-04-25 22:50:58 -0600

    Prep 3 and 4 assignments

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Set up variables. Initialize playAgain to true to run through the first set.
+        String inputPlayAgain = "yes";
+        int numberGuesses = 0;
+
+        // Loop while play again is yes
+        while (inputPlayAgain == "yes")
+        {
+            //Get a random number 
+            Random randomGenerator = new Random();  
+            int magicNumber = randomGenerator.Next(1, 100);
+
+            //init the guess number
+            int guess = 0;
+
+            // loop while guess is not the magic number 
+            do
+            {
+                //Ask user for a guess. convert to int.
+                Console.Write("What is your guess? ");
+                string inputGuess = Console.ReadLine();
+                guess = int.Parse(inputGuess);
+
+                //Increment the numbr of guesses 
+                numberGuesses = numberGuesses + 1;
+
+                //Check if the guess is higher, lower or is the magic number
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it! it took you {numberGuesses} tries");
+                }
+            } while (guess != magicNumber);
+
+            // Ask the user if they want to play again
+            Console.Write("Would you like to play again? ");
+            inputPlayAgain = Console.ReadLine();
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> userNumbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int userNumber = -1;
+        int sumNumbers = 0;
+        int maxNumber = -99999;
+
+        do
+        {
+            Console.Write("Enter number? ");
+            string inputNumber= Console.ReadLine();
+            userNumber = int.Parse(inputNumber);
+        
+            if (userNumber != 0)
+            {
+                userNumbers.Add(userNumber); 
+                sumNumbers = sumNumbers + userNumber;
+                if (userNumber > maxNumber)
+                {   
+                    maxNumber = userNumber;
+                }
+            }
+            
+        } while (userNumber != 0);
+
+        double average = ((float)sumNumbers) / userNumbers.Count();
+        Console.WriteLine($"The sum is {sumNumbers}.");
+        Console.WriteLine($"The average is {average}.");
+        Console.WriteLine($"The largest number is {maxNumber}.");
+
+        Console.WriteLine("The sorted list is:");
+        userNumbers.Sort();
+        userNumbers.ForEach(Console.WriteLine);
+
     }
 }
