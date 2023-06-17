5623174bffebfe873ef0a86d95825c20f05fc6f4
sheenahamilton <sheenhamilton@gmail.com>
2023-06-16 23:06:01 -0600

    W09: Polymorphism L05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        //Square
+        Square mySquare = new Square("pink", 35);
+
+        //Rectangle
+        Rectangle myRectangle = new Rectangle("blue", 10,30);
+
+        //Circle
+        Circle myCircle = new Circle("Green", 20);
+
+        List<Shape> myShapes = new List<Shape>();
+
+        myShapes.Add(mySquare);
+        myShapes.Add(myRectangle);
+        myShapes.Add(myCircle);
+
+        myShapes.ForEach( shape => {
+            Console.WriteLine($"My shape Color: {shape.GetColor()},  Area: {shape.GetArea().ToString()}");
+        });
+
     }
 }
